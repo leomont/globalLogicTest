dbf4bed285fa4bbdc4b812d55827773b9d76e981
Leonardo Montes <leonardo.montesm@gmail.com>
2023-10-25 17:27:20 -0500

    Microservicio de prueba ping pong

--- src/main/java/com/lmontes/globallogictest/controller/pingController.java
+++ src/main/java/com/lmontes/globallogictest/controller/pingController.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class pingController {
+package com.lmontes.globallogictest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class pingController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> getEmployeeById() {
+        return ResponseEntity.ok("pong");
+    }
 }
