b71747722006b4c1da0bffb703471e58e65d2dc7
Leonardo Montes <leonardo.montesm@gmail.com>
2023-10-25 16:16:05 -0500

    first commit

---
+++ src/main/java/com/lmontes/globallogictest/GlobalLogicTestApplication.java
@@ -0,0 +1,13 @@
+package com.lmontes.globallogictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalLogicTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalLogicTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lmontes/globallogictest/GlobalLogicTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmontes.globallogictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalLogicTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
